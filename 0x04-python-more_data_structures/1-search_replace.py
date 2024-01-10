2446482e9c2b641f1e813ce461cefbdcdf9edc27
omospaddy <omospaddy@gmail.com>
2024-01-10 04:42:50 -0800

    ok

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return([replace if number == search else number for number in my_list])
+    n_list = list(my_list)
+    for i in range(len(n_list)):
+        if n_list[i] == search:
+            n_list[i] = replace
+    return n_list
