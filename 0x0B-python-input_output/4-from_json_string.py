8b40509e9541870e937c50c02a74df2d4101144c
omospaddy <omospaddy@gmail.com>
2024-02-06 14:01:48 -0800

    done

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-
-"""Defines a JSON-to-object function."""
+"""Module containing the function from_json_string"""
 import json
 
 
 def from_json_string(my_str):
-    """Return the Python object representation of a JSON string."""
-    return json.loads(my_str)
+    """Returns an object (Python data structure) represented by a JSON string.
 
+    Args:
+        my_str (str): json object to convert to Python object.
+
+    Returns:
+        type: Python object.
+    """
+    # print("type json.loads(my_str)--> {}".format(type(json.loads(my_str))))
+    # print("type my_str--> {}".format(type(my_str)))
+    return json.loads(my_str)
